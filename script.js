9179e15c9120a022deb5507d9c2d3b185b72693d
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-04-12 17:43:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-let n = Math.floor(Math.random() * 5 + 1);
+let n = Math.floor(Math.random()%5 + 1);
 let main = document.getElementsByTagName('main')[0];
 let images = document.querySelectorAll('img');
 var randomImage = document.getElementsByClassName('none')[0];
